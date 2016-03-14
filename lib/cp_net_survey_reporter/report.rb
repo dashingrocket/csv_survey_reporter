04ab950df7a944f7df00b7800f70d5e37f626b28
Jesse Bowes <jbowes@jbowes-mac.local>
2016-03-13 23:05:40 -0600

    Analysis and improvements

--- lib/cp_net_survey_reporter/report.rb
+++ lib/cp_net_survey_reporter/report.rb
@@ -1,6 +1,6 @@
 require 'csv_hasher'
-
 require 'chartkick'
+require 'set'
 
 module CpNetSurveyReporter
   class Report
@@ -11,8 +11,11 @@ module CpNetSurveyReporter
       @timestamp = Time.now.strftime('%c')
       @results = parse_results(csv_file)
       @questions = questions
+      @analysis = analysis
+      @summary = summary
 
-      puts @results.first
+      require 'pp'
+      pp @results.first
     end
 
     def parse_results(csv_file)
@@ -25,74 +28,167 @@ module CpNetSurveyReporter
           'Overall, how satisfied are you with your current internet provider?' => :pie,
           'How interested are you in higher speed internet service?' => :pie,
           'What kind of internet household do you live in?' => :pie,
+
+          "Download:What speed internet service do you subscribe to? If your service offers a guaranteed minimum and an 'up to' speed, put your 'up to' speed here" => {type: :column, display: 'Subscribed Download Speed'},
+          "Upload:What speed internet service do you subscribe to? If your service offers a guaranteed minimum and an 'up to' speed, put your 'up to' speed here" => {type: :column, display: 'Subscribed Upload Speed'},
+          "Download:What internet speed do you typically receive during the day?If you aren't sure, consider running a speedtest at http://www.speedtest.net -- you have to be at home for the numbers to be useful" => {type: :column, display: 'Daytime Download Speed'},
+          "Upload:What internet speed do you typically receive during the day?If you aren't sure, consider running a speedtest at http://www.speedtest.net -- you have to be at home for the numbers to be useful" => {type: :column, display: 'Daytime Upload Speed'},
+          "Download:What internet speed do you typically receive during the evening?If you aren't sure, consider running a speedtest at http://www.speedtest.net -- you have to be at home for the numbers to be useful" => {type: :column, display: 'Evening Download Speed'},
+          "Upload:What internet speed do you typically receive during the evening?If you aren't sure, consider running a speedtest at http://www.speedtest.net -- you have to be at home for the numbers to be useful" => {type: :column, display: 'Evening Upload Speed'},
+
           # TODO DOWNLOAD/UPLOAD Comparison
-          # TODO Download/Upload Raw Data,
           'How much do you currently pay for internet service per month?' => :pie,
           'Do you require internet at home for your job, education, etc?' => :pie,
           'Which best describes your primary telephone service at home?' => {type: :pie, other: 'Other - Write In:Which best describes your primary telephone service at home?'},
           'What is your resident status in Crystal Park?' => :pie,
-
-          'Name:Contact' => {type: :summary, display: 'Name'},
-          'Street Address(important if you do not currently have service or are limited to satellite):Contact' => {type: :summary, display: 'Address'},
-          'Email Address(if you would like to receive updates from the internet committee):Contact' => {type: :summary, display: 'Email Address'},
-          'Additional Comments' => :summary
-      # TODO Add raw data table, too
       }
       # TODO Make this a yaml?
+
+      # TODO Satisfaction per subscriber
     end
 
-    def graph(question, type)
-      'Something'
+    def analysis
+      [
+          {
+              x: 'Who is your current internet provider?',
+              y: 'Overall, how satisfied are you with your current internet provider?',
+              type: :column,
+              title: 'Internet Company Satisfaction'
+          },
+      ]
+    end
+
+    def summary
+      {
+          'Name:Contact' => {display: 'Name'},
+          'Street Address(important if you do not currently have service or are limited to satellite):Contact' => {display: 'Address'},
+          'Email Address(if you would like to receive updates from the internet committee):Contact' => {display: 'Email Address'},
+          'Additional Comments' => {}
+      }
+    end
+
+    def chart(question, type)
       if type.is_a? Hash
         other = type[:other]
+        display = type[:display] || question
         type = type[:type]
+      else
+        other = nil
+        display = question
       end
+
+      chart = ''
 
       case type
         when :pie
-          pie_chart(count_per(question, other), {library: {title: question, width: 600} })
+          chart << pie_chart(count_per(question), {
+              library: {
+                  title: {text: display},
+                  plotOptions: {
+                      pie: {
+                          allowPointSelect: true,
+                          cursor: 'pointer',
+                          dataLabels: {
+                              enabled: false
+                          },
+                          showInLegend: true
+                      }
+                  },
+                  tooltip: {
+                      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+                  }
+              }
+          })
+
+        when :column
+          chart << column_chart(count_per(question), {library: {title: {text: display}} })
         when :summary
           # Not for graphing
         else
-          raise "unknown type #{type}"
+          chart << "unknown chart type #{type}"
+      end
+
+      chart << response_counts(other) if other
+
+      chart
+    end
+
+    def analyze_chart(analysis)
+      type = analysis[:type]
+      x = analysis[:x]
+      y = analysis[:y]
+      title = analysis[:title]
+
+      case type
+        when :column
+          column_chart(x_per_y(x, y), {library: {title: {text: title}}} )
+        else
+          "unknown type #{type}"
       end
     end
 
-    def summarize
-      summary = '<div class="summary-section">'
+    def x_per_y(x, y)
+      x_axis = Set.new
+
+      data = {}
+
       @results.each do |result|
-        summary << '<div class="summary-item">'
-        questions.each do |question, type|
-          if type.is_a? Hash
-            display = type[:display] || question
-            type = type[:type]
-          else
-            display = question
-          end
-          if type == :summary
-            summary << "<div class=\"summary-item-label\">#{display}</div>"
-            summary << "<div class=\"summary-item-value\"'>#{result[question]}</div>"
-          end
-        end
-        summary << '</div>'
+        x_axis << result[x]
+        data[result[y]] ||= {}
+        data[result[y]][result[x]] ||= 0
+        data[result[y]][result[x]] += 1
       end
-      summary << '</div>'
-      summary
+
+      x_axis = x_axis.to_a.sort
+
+      array_data = []
+
+      data.each do |x_local, y_local|
+        data_local = []
+
+        x_axis.each do |x_item|
+          y_local[x_item] ||= 0
+          data_local << [x_item, y_local[x_item]]
+        end
+
+        array_data << {name: x_local, data: data_local}
+      end
+
+      array_data
     end
 
-    def count_per(question, other = nil)
+    def response_counts(other)
+      output = '<div class="response-count">'
+      output << "<div class=\"response-count-title\">#{other}</div>"
+      counts = count_per(other)
+      counts.each do |response, count|
+        next if response.empty?
+        output << "<div class=\"response-count-row\">"
+        output << "<div class=\"response-count-label\">#{response}</div>"
+        output << "<div class=\"response-count-value\">#{count}</div>"
+        output << '</div>'
+      end
+      output << '</div>'
+    end
+
+    def summarize(result)
+      out = ''
+        @summary.each do |question, details|
+          display = details[:display] || question
+          out << "<div class=\"summary-item-label\">#{display}</div>"
+          out << "<div class=\"summary-item-value\"'>#{result[question]}</div>"
+        end
+      out
+    end
+
+    def count_per(question)
       counts = {}
       @results.each do |result|
-
-        if other && result[other] && result[other].length > 0
-          response = result[other]
-        else
-          response = result[question]
-        end
+        response = result[question]
         counts[response] ||= 0
         counts[response] += 1
       end
-      counts
+      counts.sort.to_h
     end
 
     def get_binding
